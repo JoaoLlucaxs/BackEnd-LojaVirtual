f018978d689cb9f222539edb836dd7ea8513e793
JoaoLlucaxs <queirozjoaolucas8@gmail.com>
2024-04-30 14:46:48 -0300

    ...

--- src/main/java/jl/software/lojavirtual/loja_virtual_mentoria/model/MarcaProduto.java
+++ src/main/java/jl/software/lojavirtual/loja_virtual_mentoria/model/MarcaProduto.java
@@ -1,18 +1,16 @@
 package jl.software.lojavirtual.loja_virtual_mentoria.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1) // define uma sequência de banco de dados personalizada para geração de valores de chave primária em uma entidade JPA
 public class MarcaProduto implements Serializable {
     private static final long serialVersionUID=1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
     private Long id;
     private String nomeDesc;
 
