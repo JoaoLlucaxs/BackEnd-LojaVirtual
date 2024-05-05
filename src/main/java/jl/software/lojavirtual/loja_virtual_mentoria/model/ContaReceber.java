3010b78eb0bbae174b750ba9f14a90088bd8166e
JoaoLlucaxs <queirozjoaolucas8@gmail.com>
2024-05-05 01:40:47 -0300

    ...

---
+++ src/main/java/jl/software/lojavirtual/loja_virtual_mentoria/model/ContaReceber.java
@@ -0,0 +1,115 @@
+package jl.software.lojavirtual.loja_virtual_mentoria.model;
+
+import jl.software.lojavirtual.loja_virtual_mentoria.enums.StatusContaReceber;
+import jl.software.lojavirtual.loja_virtual_mentoria.enums.TipoEndereco;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "conta_receber")
+@SequenceGenerator(name = "seq_conta_receber",sequenceName = "seq_conta_receber", initialValue = 1,allocationSize = 1)
+public class ContaReceber implements Serializable {
+
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_conta_receber")
+    private Long id;
+
+    private String descricao;
+    @Enumerated(EnumType.STRING)
+    private StatusContaReceber statusContaReceber;
+
+    @Temporal(TemporalType.DATE)
+    private Date data_Vencimento;
+    @Temporal(TemporalType.DATE)
+    private Date data_Pagamento;
+
+    private BigDecimal valortTotal;
+    private BigDecimal valorDesconto;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_id",nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+   private Pessoa pessoa;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public StatusContaReceber getStatusContaReceber() {
+        return statusContaReceber;
+    }
+
+    public void setStatusContaReceber(StatusContaReceber statusContaReceber) {
+        this.statusContaReceber = statusContaReceber;
+    }
+
+    public Date getData_Vencimento() {
+        return data_Vencimento;
+    }
+
+    public void setData_Vencimento(Date data_Vencimento) {
+        this.data_Vencimento = data_Vencimento;
+    }
+
+    public Date getData_Pagamento() {
+        return data_Pagamento;
+    }
+
+    public void setData_Pagamento(Date data_Pagamento) {
+        this.data_Pagamento = data_Pagamento;
+    }
+
+    public BigDecimal getValortTotal() {
+        return valortTotal;
+    }
+
+    public void setValortTotal(BigDecimal valortTotal) {
+        this.valortTotal = valortTotal;
+    }
+
+    public BigDecimal getValorDesconto() {
+        return valorDesconto;
+    }
+
+    public void setValorDesconto(BigDecimal valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContaReceber that = (ContaReceber) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
