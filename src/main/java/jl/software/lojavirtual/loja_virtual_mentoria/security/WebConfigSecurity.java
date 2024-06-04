100cac3ebe5e583132860c626c3d1b64613634c3
JoaoLlucaxs <queirozjoaolucas8@gmail.com>
2024-06-03 22:44:03 -0300

    ..

--- src/main/java/jl/software/lojavirtual/loja_virtual_mentoria/security/WebConfigSecurity.java
+++ src/main/java/jl/software/lojavirtual/loja_virtual_mentoria/security/WebConfigSecurity.java
@@ -1,12 +1,17 @@
 package jl.software.lojavirtual.loja_virtual_mentoria.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import jl.software.lojavirtual.loja_virtual_mentoria.service.ImplementacaoUserDetailsService;
 
 import javax.servlet.http.HttpSessionListener;
 
@@ -15,6 +20,15 @@ import javax.servlet.http.HttpSessionListener;
 @EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true)
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener {
 
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailService;
+	
+	// Irá consultar o usuário no banco com Spring Security
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(implementacaoUserDetailService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso","/deletarAcesso")
