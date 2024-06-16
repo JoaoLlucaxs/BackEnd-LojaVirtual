ad849651c749d93f297aecba6475d5f47323aeb5
JoaoLlucaxs <queirozjoaolucas8@gmail.com>
2024-06-16 11:39:42 -0300

    ..

--- src/main/java/jl/software/lojavirtual/loja_virtual_mentoria/ApplicationContextLoad.java
+++ src/main/java/jl/software/lojavirtual/loja_virtual_mentoria/ApplicationContextLoad.java
@@ -4,7 +4,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ApplicationContextLoad implements ApplicationContextAware{
 
 	// Foi criada para pegar o contexto de dependência do spring.. apenas uma classe utilitaria devido talvez não conseguir injetar diretamente
@@ -12,6 +14,7 @@ public class ApplicationContextLoad implements ApplicationContextAware{
 	@Autowired
 	private static ApplicationContext applicationContext;
 	
+	@SuppressWarnings("static-access")
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		// TODO Auto-generated method stub
--- src/main/java/jl/software/lojavirtual/loja_virtual_mentoria/LojaVirtualMentoriaApplication.java
+++ src/main/java/jl/software/lojavirtual/loja_virtual_mentoria/LojaVirtualMentoriaApplication.java
@@ -16,7 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class LojaVirtualMentoriaApplication {
 
     public static void main(String[] args) {
-    	System.out.println(new BCryptPasswordEncoder().encode("123"));
+    	//System.out.println(new BCryptPasswordEncoder().encode("123"));
         SpringApplication.run(LojaVirtualMentoriaApplication.class, args);
     }
 
